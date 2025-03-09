b242bbe308eeac071cd86b5cad37e07d75feebcf
Miguel Ballesteros <git@miballe.dev>
2025-03-09 09:20:08 -0500

    Updated to use Gemini

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 import requests as r
+import json
+from google import genai
+from google.genai import types
 
 # Authentication functions
 
-def get_bearer_token(client_id, username, password, auth_url):
+def get_bearer_token(client_id, username, password, auth_url) -> str:
     bearer_token = None
 
     id_token_payload = {
@@ -37,3 +40,54 @@ def get_bearer_token(client_id, username, password, auth_url):
             bearer_token = response_body['access_token']
 
     return bearer_token
+
+
+def gemini_generate(gemini_prompt, gproject, glocation) -> str:
+  
+    client = genai.Client(
+        vertexai=True,
+        project=gproject,
+        location=glocation,
+    )
+
+    text1 = types.Part.from_text(text=json.dumps(gemini_prompt))
+
+    model = "gemini-2.0-flash-001"
+    contents = [
+        types.Content(
+        role="user",
+        parts=[
+            text1
+          ]
+        )
+    ]
+    
+    generate_content_config = types.GenerateContentConfig(
+        temperature = 0.2,
+        top_p = 0.95,
+        max_output_tokens = 8192,
+        response_modalities = ["TEXT"],
+        safety_settings = [types.SafetySetting(
+        category="HARM_CATEGORY_HATE_SPEECH",
+        threshold="OFF"
+        ),types.SafetySetting(
+        category="HARM_CATEGORY_DANGEROUS_CONTENT",
+        threshold="OFF"
+        ),types.SafetySetting(
+        category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        threshold="OFF"
+        ),types.SafetySetting(
+        category="HARM_CATEGORY_HARASSMENT",
+        threshold="OFF"
+        )],
+    )
+
+    response = ""
+    for chunk in client.models.generate_content_stream(
+        model = model,
+        contents = contents,
+        config = generate_content_config,
+        ):
+        response += chunk.text
+        
+    return response
